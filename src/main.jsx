1d26677d16c85b67d42eae134bef41a8d11b9da1
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-13 12:35:37 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter ">
+      <BrowserRouter>
         <ModalProvider>
           <ModalManager />
           <App />
