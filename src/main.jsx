a5884e734b400b475a190cf8389f695fce46dd32
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-13 12:34:21 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/fs-76-3-react-node-healthy-hub/">
+      <BrowserRouter ">
         <ModalProvider>
           <ModalManager />
           <App />
